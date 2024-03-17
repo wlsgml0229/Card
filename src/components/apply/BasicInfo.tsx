31c0503b7f0bdaeddca38f958d5bd8d9bd3cbda1
wlsgml0229 <wlsgml0229@naver.com>
2024-03-17 12:26:17 +0900

    ✨ 카드신청폼 추가

---
+++ src/components/apply/BasicInfo.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function BasicInfo() {
+  return <div>BasicInfo</div>
+}
--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -1,5 +1,18 @@
 import React from "react"
+import { useState } from "react"
+
+import Terms from "@components/apply/Terms"
+import BasicInfo from "@components/apply/BasicInfo"
+import CardInfo from "@components/apply/CardInfo"
 
 export default function ApplyPage() {
-  return <div>Apply</div>
+  const [step, setStep] = useState(0)
+
+  return (
+    <div>
+      {step === 0 ? <Terms /> : null}
+      {step === 1 ? <BasicInfo /> : null}
+      {step === 2 ? <CardInfo /> : null}
+    </div>
+  )
 }
