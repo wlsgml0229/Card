b9044039c4c4e07983deecd50c77b5537b84075e
wlsgml0229 <wlsgml0229@naver.com>
2024-03-17 13:02:37 +0900

    ✨ 약관 제목 , 체크여부 추가

--- src/components/apply/CardInfo.tsx
+++ src/components/apply/CardInfo.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react"
 
 export default function CardInfo() {
-  return (
-    <div>CardInfo</div>
-  )
+  return <div>CardInfo</div>
 }
--- src/components/apply/Terms.tsx
+++ src/components/apply/Terms.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import Agreement from "../shared/Agreement"
 
 export default function Terms() {
   return (
-    <div>Terms</div>
+    <Agreement>
+      <Agreement.Title
+        checked={false}
+        onChange={(e, checked) => {
+          console.log(e, checked)
+        }}
+      >
+        약관에 모두 동의
+      </Agreement.Title>
+    </Agreement>
   )
 }
